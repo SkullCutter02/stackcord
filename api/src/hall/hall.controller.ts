5eb02bd86ef388d7528067064566eb88dcb479ad
skullcutter02 <=>
2021-11-15 21:25:06 +0800

    Created join hall route

--- api/src/hall/hall.controller.ts
+++ api/src/hall/hall.controller.ts
@@ -5,6 +5,7 @@ import { JwtAuthGuard } from "../auth/guards/jwt.guard";
 import { ReqWithUser } from "../types/reqWithUser.interface";
 import { CreateHallDto } from "./dto/createHall.dto";
 import { PatchHallDto } from "./dto/patchHall.dto";
+import { MakeUserTeacherDto } from "./dto/makeUserTeacher.dto";
 
 @Controller("hall")
 export class HallController {
@@ -18,13 +19,19 @@ export class HallController {
   @Get()
   @UseGuards(JwtAuthGuard)
   getUserHalls(@Req() req: ReqWithUser) {
-    return this.hallService.getUserHalls(req.user);
+    return this.hallService.getUserHalls(req.user.id);
   }
 
   @Post()
   @UseGuards(JwtAuthGuard)
   createHall(@Req() req: ReqWithUser, @Body() createHallDto: CreateHallDto) {
     return this.hallService.createHall(createHallDto, req.user);
+  }
+
+  @Post("/:id/join")
+  @UseGuards(JwtAuthGuard)
+  joinHall(@Param("id", ParseUUIDPipe) hallId: string, @Req() req: ReqWithUser) {
+    return this.hallService.joinHall(hallId, req.user);
   }
 
   @Patch("/:id")
@@ -35,5 +42,15 @@ export class HallController {
     @Body() patchHallDto: PatchHallDto
   ) {
     return this.hallService.editHall(hallId, req.user, patchHallDto);
+  }
+
+  @Patch("/:id/teacher")
+  @UseGuards(JwtAuthGuard)
+  makeUserTeacher(
+    @Param("id", ParseUUIDPipe) hallId: string,
+    @Req() req: ReqWithUser,
+    @Body() makeUserTeacherDto: MakeUserTeacherDto
+  ) {
+    return this.hallService.makeUserTeacher(hallId, req.user, makeUserTeacherDto);
   }
 }
--- api/src/hall/hall.service.ts
+++ api/src/hall/hall.service.ts
@@ -6,6 +6,7 @@ import { generateAlphanumericString } from "../utils/generateAlphanumericString"
 import { Hall } from "./models/hall.model";
 import { HallUser } from "./models/hallUser.model";
 import { PatchHallDto } from "./dto/patchHall.dto";
+import { MakeUserTeacherDto } from "./dto/makeUserTeacher.dto";
 
 @Injectable()
 export class HallService {
@@ -18,10 +19,10 @@ export class HallService {
     return this.hallModel.query().findById(hallId).withGraphFetched("users");
   }
 
-  public async getUserHalls(user: User) {
+  public async getUserHalls(userId: string) {
     return this.hallUserModel
       .query()
-      .where({ userId: user.id })
+      .where({ userId })
       .innerJoin("halls", "halls.id", "=", "halls_users.hall_id")
       .select("*");
   }
@@ -37,9 +38,30 @@ export class HallService {
       .withGraphJoined("[hall, user]");
   }
 
+  public async joinHall(hallId: string, user: User) {
+    await this.isUserInHall(hallId, user.id);
+    await this.hallUserModel.query().insert({ role: "student", hallId, userId: user.id });
+    return this.hallUserModel.query().findOne({ hallId, userId: user.id }).withGraphJoined("[hall, user]");
+  }
+
   public async editHall(hallId: string, user: User, { anonymous }: PatchHallDto) {
     await this.isTeacherInHall(hallId, user.id);
     return this.hallModel.query().patchAndFetchById(hallId, { anonymous });
+  }
+
+  public async makeUserTeacher(hallId: string, user: User, { userId: targetUserId }: MakeUserTeacherDto) {
+    await this.isTeacherInHall(hallId, user.id);
+
+    await this.hallUserModel.query().where({ hallId: hallId, userId: targetUserId }).patch({ role: "teacher" });
+    return this.getHall(hallId);
+  }
+
+  private async isUserInHall(hallId: string, userId: string) {
+    const hall = await this.getHall(hallId);
+    const isUserInHall = hall.users.some((hallUser) => hallUser.id === userId);
+
+    if (isUserInHall) throw new UnauthorizedException("User is already in hall");
+    return hall;
   }
 
   private async isTeacherInHall(hallId: string, userId: string) {
@@ -49,7 +71,7 @@ export class HallService {
       (hallUser: User & { role: string }) => hallUser.id === userId && hallUser.role === "teacher"
     );
 
-    if (!isTeacherInHall) throw new UnauthorizedException();
+    if (!isTeacherInHall) throw new UnauthorizedException("User is not a teacher in this hall");
 
     return hall;
   }
