0c07f8e69e43e56f4eca773ff1208f91ea0c4af1
skullcutter02 <=>
2021-11-16 19:40:30 +0800

    Created get question route

--- api/src/question/question.controller.ts
+++ api/src/question/question.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Req, UseGuards } from "@nestjs/common";
 
 import { QuestionService } from "./question.service";
 import { JwtAuthGuard } from "../auth/guards/jwt.guard";
@@ -8,6 +8,11 @@ import { CreateQuestionDto } from "./dto/createQuestion.dto";
 @Controller("question")
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
+
+  @Get("/:id")
+  getQuestion(@Param("id", ParseUUIDPipe) questionId: string) {
+    return this.questionService.getQuestion(questionId);
+  }
 
   @Post()
   @UseGuards(JwtAuthGuard)
