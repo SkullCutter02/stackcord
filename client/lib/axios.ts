5a29979ded36d8fef6aa602bdb6ad1ca601301bf
skullcutter02 <=>
2021-11-19 20:37:04 +0800

    Added axios refresh token interceptor

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import createAuthRefreshInterceptor from "axios-auth-refresh";
+
 import HOST from "../constants/host";
 
 const instance = axios.create({
@@ -8,6 +10,18 @@ const instance = axios.create({
   },
   responseType: "json",
   withCredentials: true,
+});
+
+const refreshAuthLogic = (failedRequest) => {
+  if (failedRequest.response.data.message === "Unauthorized") {
+    return instance.get("auth/refresh").then();
+  } else {
+    return Promise.resolve();
+  }
+};
+
+createAuthRefreshInterceptor(instance, refreshAuthLogic, {
+  pauseInstanceWhileRefreshing: true,
 });
 
 export { instance as axios };
