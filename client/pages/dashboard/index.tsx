b5597a085aaa79097a5af9f1619bc3b7c11bcdbe
skullcutter02 <=>
2021-11-20 00:23:32 +0800

    Made it when user has a hall, it will redirect them to the first hall

--- client/pages/dashboard/index.tsx
+++ client/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useQuery } from "react-query";
+import { useRouter } from "next/router";
 
 import IHall from "../../types/hall.interface";
 import getHalls from "../../queries/getHalls";
@@ -9,6 +10,14 @@ const DashboardPage: React.FC = () => {
   const { isLoading, data: halls } = useQuery<IHall[]>("halls", () =>
     getHalls()
   );
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && halls.length > 0) {
+      router.push(`/dashboard/hall/${halls[0].id}`);
+    }
+  }, [halls]);
 
   return <>{!isLoading && halls.length === 0 && <NoHall />}</>;
 };
