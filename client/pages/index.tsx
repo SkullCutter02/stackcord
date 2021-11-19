9cd0e6243bb55ff960298228f518a4c069d954b4
skullcutter02 <=>
2021-11-19 20:22:54 +0800

    Will now redirect to dashboard when you have an access token

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
 import HomePageContainer from "../components/ui/home/HomePageContainer";
+import { axios } from "../lib/axios";
 
 const HomePage: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    axios
+      .get("auth/me")
+      .then(({ data }) => {
+        if (data) {
+          router.push("/dashboard").then();
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <HomePageContainer />
