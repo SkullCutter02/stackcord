6d0a21c61d29f6bf609dff7ba9cb80a69de05cd8
skullcutter02 <=>
2021-11-19 19:33:24 +0800

    Set up cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,7 +11,13 @@ import { AppModule } from "./app.module";
 const PORT = process.env.PORT || 5000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
+      credentials: true,
+    },
+    logger: true,
+  });
 
   app.use(cookieParser());
   app.use(helmet());
