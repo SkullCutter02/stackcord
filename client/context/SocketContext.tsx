7971fb704dd0fdeeb6fd2e5dc74039a6f90c5064
skullcutter02 <=>
2021-12-11 19:10:02 +0800

    Fixed websocket client integration

--- client/context/SocketContext.tsx
+++ client/context/SocketContext.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import io from "socket.io-client";
 
 import HOST from "../constants/host";
 import { createContext } from "react";
 
-const socket = io.connect(HOST);
 export const SocketContext = createContext<SocketIOClient.Socket>(null);
 
 export const SocketProvider: React.FC = ({ children }) => {
+  const socket = useMemo(() => (typeof window !== "undefined" ? io.connect(HOST) : null), []);
+
+  useEffect(() => {
+    return () => {
+      socket?.close();
+    };
+  }, []);
+
   return (
     <>
       <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
